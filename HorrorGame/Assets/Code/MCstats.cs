bb631560b2b8a488e052d77f7876833d579eecac
Kagan <lundeat@gmail.com>
2019-02-19 11:15:47 -0800

    Main Character(MC)
    added in a code file attached to the "main character". This file will contain the stats on the main character which we can access through the object. Please place any variables that define the main character in that file

---
+++ HorrorGame/Assets/Code/MCstats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCstats : MonoBehaviour
+{
+    float Health = 100;
+    float Stamina = 100;
+    float Agility = 1;
+    float Strength = 1;
+    float Constitution = 1;
+    float Sanity = 1;
+}
