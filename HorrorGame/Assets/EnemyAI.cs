a6a2d711cc78c07284bc0fea5151321cf62153a7
Jack Davies <shadowjmdavies@gmail.com>
2019-03-01 10:01:47 -0800

    EnemyAI 1
    Added basic player "model" and a very basic sample enemy with basic AI.

---
+++ HorrorGame/Assets/EnemyAI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyAI : MonoBehaviour
+{
+    Transform player;               // Reference to the player's position.
+    public NavMeshAgent nav;               // Reference to the nav mesh agent.
+    public float fieldOfViewAngle = 110f;
+    public bool playerInSight;
+    public Vector3 personalLastSighting;
+    public SphereCollider Collider;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    void Update()
+    {
+        nav.SetDestination(player.position);
+        
+    }
+}
---
+++ HorrorGame/Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 5, 0f, Input.GetAxis("Vertical") * Time.deltaTime * 5);
+    }
+}
