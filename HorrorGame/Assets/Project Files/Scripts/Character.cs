b6d2e59c302012fccbd8a3403e4a2694ccdb012d
Dog Shit Burritos <vonsmith01@gmail.com>
2019-04-15 11:30:08 -0700

    Latest Build
    Stuff for the presentation

---
+++ HorrorGame/Assets/Project Files/Scripts/Character.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float health;
+    public Text Healthbar;
+
+    void Start()
+    {
+        health = 100;
+    }
+
+    void Update()
+    {
+        Healthbar.text = health.ToString();
+    }
+}
---
+++ HorrorGame/Assets/Project Files/Scripts/EnemyCollision.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+    float counter = 0;
+
+    public float health = 3;
+    public GameObject Enemy;
+
+    private void FixedUpdate()
+    {
+        if(health <= 0)
+        {
+            Destroy(Enemy);
+        }
+    }
+
+    private void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("test");
+        if (other.tag == "Player")
+        {
+            GameObject.Find("Character").GetComponent<Character>().health -= 16;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("test");
+        if (other.tag == "Player")
+        {
+            if (counter > 49)
+            {
+                GameObject.Find("Character").GetComponent<Character>().health -= 16;
+                counter = 0;
+            }
+            else
+            {
+                counter += 1;
+            }
+        }
+    }
+}
