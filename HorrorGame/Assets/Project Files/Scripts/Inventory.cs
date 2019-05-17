336dff0234f89b866cfcf79d8bf7c30a54863d65
Dog Shit Burritos <vonsmith01@gmail.com>
2019-05-17 12:40:26 -0700

    Horror Game  V0.01
    Hell cometh

---
+++ HorrorGame/Assets/Project Files/Scripts/Inventory.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject inventroy;
+    private bool inventoryEnabled;
+
+    private int allSlots;
+    private int enabledSlots;
+    private GameObject[] slot;
+
+    public GameObject ItemPanels;
+
+    void Start()
+    {
+        allSlots = 3;
+        slot = new GameObject[allSlots];
+        for (int i = 0; i < allSlots; i++)
+        {
+            slot[i] = ItemPanels.transform.GetChild(i).gameObject;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            inventoryEnabled = !inventoryEnabled;
+
+        if (inventoryEnabled == true)
+        {
+            inventroy.SetActive(true);
+        }
+        else
+        {
+            inventroy.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            GameObject itemPickedUp = other.gameObject;
+            Item item = itemPickedUp.GetComponent<Item>();
+
+            AddItem(item.ID, item.type, item.description, item.icon);
+        }
+    }
+
+    void AddItem(int itemID, string itemType, string itemDescription, Texture2D itemIcon)
+    {
+        for(int i = 0; i < allSlots; i++)
+        {
+
+        }
+    }
+}
